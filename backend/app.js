4f4e28dee87938e1ee84208bb8921f2ef6bddd93
Michel Lopes <lopesmichel.lp@gmail.com>
2025-05-24 11:02:06 -0300

    conectando na api

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,20 @@
+require ("dotenv").config()
+
 const express = require ("express")
 const path = require("path")
 const cors = require("cors")
 
-const port = 5000
+const port = process.env.PORT
 
 const app = express()
 
 //config JSON e form data response
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+
+// routes
+const router = require("./routes/Router.js")
+app.use(router)
 
 app.listen(port, () => {
     console.log('app rodando na porta ${port}')
