2d9436e86f0bec486de80860db94e1d163d885ed
Michel Lopes <lopesmichel.lp@gmail.com>
2025-05-26 21:54:13 -0300

    upload de imagem

--- backend/middlewares/userValidations.js
+++ backend/middlewares/userValidations.js
@@ -41,7 +41,21 @@ const loginValidation = () => {
         body("password").isString().withMessage("A senha é obrigatória"),
     ]
 }
-module.exports = {
-    userCreateValidation,
-    loginValidation,
+
+const userUpdateValidation = () => {
+    return [
+      body("name")
+        .optional()
+        .isLength({ min: 3 })
+        .withMessage("O nome precisa de pelo menos 3 caracteres."),
+      body("password")
+        .optional()
+        .isLength({ min: 5 })
+        .withMessage("A senha precisa ter no mínimo 5 caracteres."),
+    ];
 }
+module.exports = {
+  userCreateValidation,
+  loginValidation,
+  userUpdateValidation,
+};
